023c6cda1c8fc0a5b6c5a028ffab1b1efed69cdc
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-10-09 19:27:46 +0100

    Finished basic functionality of DiceApp [Practica1]

--- Practica1/src/js/index.js
+++ Practica1/src/js/index.js
@@ -1,25 +1,30 @@
 /*** index.js ***/
-
-const btn1 = document.getElementById('button1');
-const btn2 = document.getElementById('button2');
-const btn3 = document.getElementById('button3');
-
-const span1 = document.getElementById('span1');
-const span2 = document.getElementById('span2');
-const span3 = document.getElementById('span3');
-
+// Declaracion de variables
+const btnArr = document.querySelectorAll('.box button');
+const spanArr = document.querySelectorAll('.box .dice span');
+const hist = document.querySelector('#history');
+const reset = document.querySelector('#reset');
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * max) + min;
 
-btn1.addEventListener('click', () => {
+
+
+btnArr[0].addEventListener('click', () => {
     let number = getRandomNumber(1, 6);
-    span1.textContent = number;
+    spanArr[0].textContent = number;
+    hist.textContent += `${number}   `;
 });
-btn2.addEventListener('click', () => {
+btnArr[1].addEventListener('click', () => {
     let number = getRandomNumber(1, 6);
-    span2.textContent = number;
+    spanArr[1].textContent = number;
+    hist.textContent += `${number}   `;
 });
-btn3.addEventListener('click', () => {
+btnArr[2].addEventListener('click', () => {
     let number = getRandomNumber(1, 6);
-    span3.textContent = number;
+    spanArr[2].textContent = number;
+    hist.textContent += `${number}   `;
+});
+
+reset.addEventListener('click', () => {
+    hist.textContent = '';
 });
